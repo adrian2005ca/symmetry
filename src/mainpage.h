596eaca50c1ccad7507531bd0ab2116044d13bb5
Adrian B <adrian2005ca@gmail.com>
2011-12-18 19:32:28 -0500

    Made a Main Page for the documentation

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,3 +1,19 @@
 /**
- *  Documetation for symmetry project here.
+ * @mainpage
+ * @section about About symmetry
+ * 
+ * Symmetry is a project for computing canonical representatives of orbits
+ * in the metric polytope.
+ * 
+ * @section install Installing symmetry
+ * 
+ * You should be able to run the following:
+ * @code
+ * cd path-to-symmetry
+ * mkdir bin
+ * cd bin
+ * cmake ../src
+ * make
+ * make doc //optional, will make the documentation in path-to-symmetry/doc
+ * @endcode
  */
